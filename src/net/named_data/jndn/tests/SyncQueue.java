653cb31c0c1e9f4cbc2866ab91cbee2070f35f9f
gpwclark <gpwclark@gmail.com>
2018-04-30 18:29:22 +0100

    more work to genearlize chat simulation. Use logging interface and extract Chat class out of TestChronoChat class.

--- src/net/named_data/jndn/tests/SyncQueue.java
+++ src/net/named_data/jndn/tests/SyncQueue.java
@@ -7,15 +7,22 @@ import java.util.logging.Logger;
 
 public class SyncQueue<T> {
 	LinkedBlockingQueue<T> queue;
+	private final long timeout;
+
+	public SyncQueue(long timeout) {
+		this.queue = new LinkedBlockingQueue<>();
+		this.timeout = timeout;
+	}
 
 	public SyncQueue() {
 		this.queue = new LinkedBlockingQueue<>();
+		timeout = 5l;
 	}
 
 	public T deQ() {
 		T data = null;
 		try {
-			data = (T)this.queue.poll(5, TimeUnit.SECONDS);
+			data = (T)this.queue.poll(timeout, TimeUnit.SECONDS);
 		} catch (InterruptedException e) {
 			Logger.getLogger(Chat.class.getName()).log(Level.SEVERE,
 				"interrupted while waiting on Q'd stuff", e);
